2f0682f8357d32a1878605bf812b23e6965ca439
MishaRiko <bmishariko@gmail.com>
2025-10-05 06:00:48 +0300

    finish tasks

---
+++ src/task-5.ts
@@ -0,0 +1,12 @@
+type status1 = 'loading' | 'success' | 'error';
+function logStatus(status: status1): void {
+  if (status === 'loading') {
+    console.log('Loading...');
+  } else if (status === 'success') {
+    console.log('Success!');
+  } else if (status === 'error') {
+    console.log('Something went wrong');
+  }
+}
+
+logStatus('loading');
